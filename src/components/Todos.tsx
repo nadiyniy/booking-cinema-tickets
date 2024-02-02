ae31081b2ab2bd178063dc0d746f54279f8b2ab0
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2024-02-02 19:13:58 +0200

    add DataGrid & markup todos

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,84 @@
-import { Box, Button, Container, Paper, TextField, Typography } from '@mui/material';
+import { Box, Button, Checkbox, Container, Paper, TextField, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import SearchIcon from '@mui/icons-material/Search';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
+import { makeRequest } from '../services/apiTodos';
+import { Favorite, FavoriteBorder } from '@mui/icons-material';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+const columns: GridColDef[] = [
+    {
+        field: 'favorite',
+        headerName: '',
+        width: 60,
+        sortable: false,
+        filterable: false,
+        groupable: false,
+        hideable: false,
+
+        renderCell: (params) => (
+            <Checkbox checked={params.row.completed} {...label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+        )
+    },
+    {
+        field: 'delete',
+        headerName: '',
+        width: 100,
+        sortable: false,
+        filterable: false,
+        groupable: false,
+        hideable: false,
+
+        renderCell: (params) => (
+            <Button variant="outlined" onClick={() => console.log(params, 123)}>
+                Delete
+            </Button>
+        )
+    },
+    { field: 'id', headerName: 'ID', width: 50 },
+    { field: 'title', headerName: 'Todo', width: 300 },
+    { field: 'name', headerName: 'Name', width: 130 },
+    { field: 'phone', headerName: 'Phone', width: 150 },
+    { field: 'email', headerName: 'Email', description: 'This is email', sortable: false, width: 160 }
+];
 
 const Todos = () => {
     const [newTodo, setNewTodo] = useState('');
     const [foundTodo, setFoundTodo] = useState('');
+    const [allTodos, setAllTodos] = useState([]);
+    console.log(allTodos);
+
+    const rows = allTodos.map((todo) => ({
+        delete: 'delete',
+        completed: todo.completed,
+        id: todo.id,
+        name: todo.user.name,
+        title: todo.title,
+        phone: todo.user.phone,
+        email: todo.user.email
+    }));
+    useEffect(() => {
+        makeRequest(`query allTodos{
+  todos{
+  data{
+    title
+    completed
+    
+    id
+    user{
+      id
+      email
+      name
+      phone
+    }
+  }
+    }
+
+}`).then((res) => setAllTodos(res.data.todos.data));
+    }, []);
 
     const handleCreateTodo = (event: any) => {
         event.preventDefault();
@@ -88,14 +161,17 @@ const Todos = () => {
                 </Box>
             </Box>
             <Paper>
-                <ul>
-                    <li>12</li>
-                    <li>12</li>
-                    <li>12</li>
-                    <li>12</li>
-                    <li>12</li>
-                    <li>12</li>
-                </ul>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                        pagination: {
+                            paginationModel: { page: 0, pageSize: 5 }
+                        }
+                    }}
+                    pageSizeOptions={[5, 10, 20, 100]}
+                    autoHeight
+                />
             </Paper>
         </Container>
     );
---
+++ src/services/apiTodos.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const todosApi = axios.create({
+    baseURL: 'https://graphqlzero.almansi.me/api'
+});
+
+export const makeRequest = async (query: any) => {
+    try {
+        const { data } = await todosApi.post('', {
+            query: query
+        });
+        return data;
+    } catch (error) {
+        console.error('GraphQL request failed:', error);
+    }
+};
